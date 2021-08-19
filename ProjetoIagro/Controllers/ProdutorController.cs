ef10281f6655b074047079ab242d1397f16310a0
MariaVictoria00 <mariavictoriapaybe@gmail.com>
2021-08-19 01:24:04 -0300

    endpoint produtor

---
+++ ProjetoIagro/Controllers/ProdutorController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProjetoIagro.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoIagro.Controllers
+{
+    public class ProdutorController : Controller
+    {
+     
+        private readonly Contexto contexto;
+        private Contexto _contexto;
+        public ProdutorController(Contexto contexto)
+        {
+            _contexto = contexto;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _contexto.Produtor.ToListAsync());
+        }
+
+        [HttpGet]
+        public ActionResult CadastrarProdutor()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CadastrarProdutor(Produtor produtor)
+        {
+            if (ModelState.IsValid)
+            {
+                _contexto.Add(produtor);
+                await _contexto.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            else
+            {
+                return View(produtor);
+            }
+        }
+    }
+}
---
+++ ProjetoIagro/Models/Produtor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoIagro.Models
+{
+    public class Produtor
+    {
+
+        public Int32 ProdutorID { get; set; }
+        public String Nome { get; set; }
+        public String CPF { get; set; }
+        public String Rua { get; set; }
+        public String Numero { get; set; }
+        public Int32 Municipio { get; set; }
+    }
+}
